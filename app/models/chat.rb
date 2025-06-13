cc0c2aaeeeb0edca842b508b450eed73120c7084
Martinfigue123 <mfigueroa5@miuandes.cl>
2025-06-13 12:32:23 -0400

    Lab 10

--- app/models/chat.rb
+++ app/models/chat.rb
@@ -1,6 +1,6 @@
 class Chat < ApplicationRecord
     belongs_to :sender,  class_name: 'User'
     belongs_to :receiver, class_name: 'User'
-    has_many :messages
+    has_many :messages, dependent: :destroy
     validates :sender_id, :receiver_id, presence: true
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,9 @@ Rails.application.routes.draw do
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
-  resources :chats
-  resources :messages
+  resources :chats do
+    resources :messages, only: [:new, :create, :index]
+  end
   devise_for :users
   resources :users, only: [:index, :show, :new, :create, :edit, :destroy]
   root "users#index"
